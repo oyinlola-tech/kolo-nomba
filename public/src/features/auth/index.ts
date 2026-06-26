279eb0fbb63faf1ea24120405a6c4bb6cfc6d3d5
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-06-26 16:42:04 -0700

    refactor: remove RegisterCoopPage export

--- public/src/features/auth/index.ts
+++ public/src/features/auth/index.ts
@@ -1,4 +1,3 @@
 export { LoginPage } from "./pages/login.page";
 export { RegisterPage } from "./pages/register.page";
-export { RegisterCoopPage } from "./pages/register-cooperative.page";
 export { VerifyOTPPage } from "./pages/verify-otp.page";
