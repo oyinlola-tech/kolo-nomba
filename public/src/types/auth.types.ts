8132f2c8fc701d8338359e08175e5ecb0856de79
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-06-26 13:51:15 -0700

    Add auth types

---
+++ public/src/types/auth.types.ts
@@ -0,0 +1,33 @@
+export type UserRole = "SUPER_ADMIN" | "GROUP_ADMIN" | "MEMBER";
+
+export interface AuthUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  role: UserRole;
+  status: string;
+}
+
+export interface AuthTokens {
+  accessToken: string;
+}
+
+export interface AuthSession {
+  user: AuthUser;
+  accessToken: string;
+}
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface RegisterPayload {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+}
