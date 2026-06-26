a538d011a0a1c0d39b1eb5751b519340f1997392
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-06-26 16:29:58 -0700

    fix: restore RegisterCoopPage export name

--- public/src/features/auth/index.ts
+++ public/src/features/auth/index.ts
@@ -1,4 +1,4 @@
 export { LoginPage } from "./pages/login.page";
 export { RegisterPage } from "./pages/register.page";
-export { RegisterCooperativePage } from "./pages/register-cooperative.page";
+export { RegisterCoopPage } from "./pages/register-cooperative.page";
 export { VerifyOTPPage } from "./pages/verify-otp.page";
