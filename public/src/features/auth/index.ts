8551909b09748a1dd2188e50f2792e75295cb235
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-06-26 16:11:04 -0700

    refactor: update auth exports, remove password-reset pages

--- public/src/features/auth/index.ts
+++ public/src/features/auth/index.ts
@@ -1,6 +1,4 @@
 export { LoginPage } from "./pages/login.page";
 export { RegisterPage } from "./pages/register.page";
-export { RegisterCoopPage } from "./pages/register-cooperative.page";
+export { RegisterCooperativePage } from "./pages/register-cooperative.page";
 export { VerifyOTPPage } from "./pages/verify-otp.page";
-export { ForgotPasswordPage } from "./pages/forgot-password.page";
-export { ResetPasswordPage } from "./pages/reset-password.page";
