3c1c52391381f74f8f9a1fc89ad4d52025cbb77c
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-06-26 11:29:02 -0700

    Add base event class

---
+++ kolo-backend/src/events/core/event.ts
@@ -0,0 +1,47 @@
+export abstract class Event {
+  public readonly name: string;
+  public readonly timestamp: Date;
+  public readonly payload: Record<string, unknown>;
+
+  constructor(name: string, payload: Record<string, unknown> = {}) {
+    this.name = name;
+    this.timestamp = new Date();
+    this.payload = payload;
+  }
+}
+
+export class UserEvent extends Event {
+  constructor(name: string, payload: Record<string, unknown>) {
+    super(`user.${name}`, payload);
+  }
+}
+
+export class GroupEvent extends Event {
+  constructor(name: string, payload: Record<string, unknown>) {
+    super(`group.${name}`, payload);
+  }
+}
+
+export class ContributionEvent extends Event {
+  constructor(name: string, payload: Record<string, unknown>) {
+    super(`contribution.${name}`, payload);
+  }
+}
+
+export class PaymentEvent extends Event {
+  constructor(name: string, payload: Record<string, unknown>) {
+    super(`payment.${name}`, payload);
+  }
+}
+
+export class PayoutEvent extends Event {
+  constructor(name: string, payload: Record<string, unknown>) {
+    super(`payout.${name}`, payload);
+  }
+}
+
+export class SecurityEvent extends Event {
+  constructor(name: string, payload: Record<string, unknown>) {
+    super(`security.${name}`, payload);
+  }
+}
