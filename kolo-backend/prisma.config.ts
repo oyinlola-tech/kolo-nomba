67d7bce5b0bebe123c09a22e987598127042c239
Oyinlola-tech <oyinlola.tech@icloud.com>
2026-06-28 08:55:01 +0100

    style: fix whitespace in prisma.config.ts

--- kolo-backend/prisma.config.ts
+++ kolo-backend/prisma.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: {
+  migrations: { 
     path: "src/database/migrations",
   },
   engine: "classic",
